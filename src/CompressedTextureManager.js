6c135d9e8f1a61baacc4be1f6224ca40610e893a
zprodev <zproject.develop@gmail.com>
2018-02-05 22:52:16 +0900

    Add etc1 support

--- src/CompressedTextureManager.js
+++ src/CompressedTextureManager.js
@@ -33,7 +33,8 @@ CompressedTextureManager.prototype.onContextChange = function() {
         dxt: getExtension(gl, "WEBGL_compressed_texture_s3tc"),
         pvrtc: getExtension(gl, "WEBGL_compressed_texture_pvrtc"),
         astc: getExtension(gl, "WEBGL_compressed_texture_astc"),
-        atc: getExtension(gl, "WEBGL_compressed_texture_atc")
+        atc: getExtension(gl, "WEBGL_compressed_texture_atc"),
+        etc1: getExtension(gl, "WEBGL_compressed_texture_etc1")
     };
     // CRN exists only with DXT!
     this.extensions.crn = this.extensions.dxt;
